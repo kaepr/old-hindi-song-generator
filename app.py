cc25af9d1f14ce22ca00f684b0db0dfa10a304da
Shagun Agrawal <51134776+standmarsh@users.noreply.github.com>
2020-07-21 23:34:45 +0530

    fixing port number

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def output_lyrics():
     return jsonify(response)
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = False, port = os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', debug = False, port = os.environ.get('PORT', 5000))
